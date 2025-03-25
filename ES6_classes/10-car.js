b94892811675e4d5624f59651bb8c315e0c85733
mansiluca <49164932+mansiluca@users.noreply.github.com>
2025-03-25 15:28:20 +0100

    Refactor Car class to use private properties and simplify cloneCar method

--- ES6_classes/10-car.js
+++ ES6_classes/10-car.js
@@ -1,44 +1,11 @@
 export default class Car {
-  constructor(brand, motor, color) {
-    this.brand = brand;
-    this.motor = motor;
-    this.color = color;
-  }
-
-  get brand() {
-    return this._brand;
-  }
-
-  set brand(newBrand) {
-    if (typeof newBrand !== 'string') {
-      throw new TypeError('Brand must be a string');
+    constructor(brand, motor, color) {
+        this._brand = brand;
+        this._motor = motor;
+        this._color = color;
     }
-    this._brand = newBrand;
-  }
 
-  get motor() {
-    return this._motor;
-  }
-
-  set motor(newMotor) {
-    if (typeof newMotor !== 'string') {
-      throw new TypeError('Motor must be a string');
+    cloneCar() {
+        return new this.constructor();
     }
-    this._motor = newMotor;
-  }
-
-  get color() {
-    return this._color;
-  }
-
-  set color(newColor) {
-    if (typeof newColor !== 'string') {
-      throw new TypeError('Color must be a string');
-    }
-    this._color = newColor;
-  }
-
-  cloneCar() {
-    return new this.constructor(this.brand, this.motor, this.color);
-  }
 }
