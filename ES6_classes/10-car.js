4e28ca3e5231b563e442f50cbe50e9a8a412d8b1
mansiluca <49164932+mansiluca@users.noreply.github.com>
2025-03-25 15:46:57 +0100

    Refactor Car class to implement Symbol.species for improved cloning functionality

--- ES6_classes/10-car.js
+++ ES6_classes/10-car.js
@@ -1,11 +1,16 @@
 export default class Car {
-    constructor(brand, motor, color) {
-        this._brand = brand;
-        this._motor = motor;
-        this._color = color;
-    }
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
 
-    cloneCar() {
-        return new this.constructor();
-    }
+  static get [Symbol.species]() {
+    return this;
+  }
+
+  cloneCar() {
+    const Species = this.constructor[Symbol.species];
+    return new Species();
+  }
 }
