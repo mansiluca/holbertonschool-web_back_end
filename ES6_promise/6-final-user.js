c7c26a251d7a0ab54e672c8a32144b2f2873bd01
mansiluca <49164932+mansiluca@users.noreply.github.com>
2025-04-01 13:51:29 +0200

    Remove handleProfileSignup function to streamline user signup process

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,6 +1,2 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
-
-export default function handleProfileSignup(firstName = '', lastName = '', fileName = '') {
-  return Promise.allSettled([uploadPhoto(fileName), signUpUser(firstName, lastName)]);
-}
