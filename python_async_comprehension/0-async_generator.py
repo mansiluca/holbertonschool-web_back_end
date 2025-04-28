2af2a1c1bb17e8cdea82161a12f010c68d69748f
mansiluca <49164932+mansiluca@users.noreply.github.com>
2025-04-28 16:30:25 +0200

    Add docstring to async_generator for clarity on functionality

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -8,6 +8,9 @@ from typing import AsyncGenerator
 
 
 async def async_generator() -> AsyncGenerator[float, None]:
+    """
+    Async generator that yields a random number between 0 and 10
+    """
     for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
