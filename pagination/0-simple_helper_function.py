42f73a9e2b86c82e63eda37ab05484985897ab25
mansiluca <49164932+mansiluca@users.noreply.github.com>
2025-04-29 10:43:20 +0200

    Fix return type annotation in index_range function for clarity

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -2,10 +2,10 @@
 """
 Module that provides a simple helper function
 """
+from typing import List, Tuple
 
-
-def index_range(page: int, page_size: int) -> tuple:
-    """
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """"
     Returns a tuple of size two containing a start index and an end index
     corresponding to the range of indexes to return for the given pagination
     parameters.
