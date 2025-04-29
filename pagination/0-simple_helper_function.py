0d6a324294a0d9829ebd584a77dbe5e0f14db849
mansiluca <49164932+mansiluca@users.noreply.github.com>
2025-04-29 10:46:25 +0200

    Fix docstring formatting and correct end index calculation in index_range function

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -6,13 +6,13 @@ import typing
 
 
 def index_range(page: int, page_size: int) -> typing.Tuple[int, int]:
-    """"
+    """
     Returns a tuple of size two containing a start index and an end index
     corresponding to the range of indexes to return for the given pagination
     parameters.
     """
-    assert isinstance(page, type(page_size)) == int
+    assert isinstance(page, int) and isinstance(page_size, int)
     assert page > 0 and page_size > 0
     start_index = (page - 1) * page_size
-    end_index = start_index * page_size
+    end_index = start_index + page_size
     return (start_index, end_index)
