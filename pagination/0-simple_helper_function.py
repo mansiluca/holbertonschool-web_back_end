e1021a35b1ade24ab9b7c8235c54346c1c621713
mansiluca <49164932+mansiluca@users.noreply.github.com>
2025-04-29 10:44:58 +0200

    Fix type hint for index_range function and correct end index calculation

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -2,9 +2,9 @@
 """
 Module that provides a simple helper function
 """
-from typing import List, Tuple
+import typing
 
-def index_range(page: int, page_size: int) -> Tuple[int, int]:
+def index_range(page: int, page_size: int) -> typing.Tuple[int, int]:
     """"
     Returns a tuple of size two containing a start index and an end index
     corresponding to the range of indexes to return for the given pagination
@@ -13,5 +13,5 @@ def index_range(page: int, page_size: int) -> Tuple[int, int]:
     assert isinstance(page, type(page_size)) == int
     assert page > 0 and page_size > 0
     start_index = (page - 1) * page_size
-    end_index = start_index + page_size
+    end_index = start_index * page_size
     return (start_index, end_index)
