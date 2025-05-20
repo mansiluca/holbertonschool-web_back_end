ea67fb081787d5cc9b86098cbec27bafbc9ed0c0
mansiluca <49164932+mansiluca@users.noreply.github.com>
2025-05-21 00:09:44 +0200

    Refactor 6-http_express.js for consistent formatting; adjust indentation in response handling and server log comment

--- Node_JS_basic/6-http_express.js
+++ Node_JS_basic/6-http_express.js
@@ -5,11 +5,11 @@ const hostname = '127.0.0.1';
 const port = 1245;
 
 app.get('/', (req, res) => {
-    res.send('Hello Holberton School!');
+  res.send('Hello Holberton School!');
 });
 
 app.listen(port, hostname, () => {
-    // console.log(`Server running at http://${hostname}:${port}/`);
+  // console.log(`Server running at http://${hostname}:${port}/`);
 });
 
 module.exports = app;
