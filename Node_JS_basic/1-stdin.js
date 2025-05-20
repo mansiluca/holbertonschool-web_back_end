c3e7526d7f23ae645d960542c922988a491c55c6
mansiluca <49164932+mansiluca@users.noreply.github.com>
2025-05-21 00:20:46 +0200

    Fix 1-stdin.js to remove newline character from name output for cleaner display

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -2,7 +2,7 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 process.stdin.on('data', (data) => {
   const name = data.toString().trim();
   if (name !== null) {
-    process.stdout.write(`Your name is: ${name}\n`);
+    process.stdout.write(`Your name is: ${name}`);
   }
 });
 process.stdin.on('end', () => {
