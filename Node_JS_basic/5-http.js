0937f0c30e59c2b5b337c0dcf9da44a620f39b20
mansiluca <49164932+mansiluca@users.noreply.github.com>
2025-05-20 15:16:51 +0200

    Refactor 5-http.js to use countStudents from external module and remove inline implementation

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,41 +1,5 @@
 const http = require('http');
-const fs = require('fs').promises;
-
-async function countStudents(path) {
-  try {
-    const data = await fs.readFile(path, 'utf8');
-    const lines = data.split('\n')
-      .filter((line) => line.trim() !== '')
-      .slice(1);
-
-    if (lines.length === 0) {
-      throw new Error('Cannot load the database');
-    }
-
-    const students = {};
-    let totalStudents = 0;
-
-    for (const line of lines) {
-      const [firstName, lastName, age, field] = line.split(',');
-      if (firstName && lastName && age && field) {
-        if (!students[field]) {
-          students[field] = [];
-        }
-        students[field].push(firstName);
-        totalStudents += 1;
-      }
-    }
-
-    let result = `Number of students: ${totalStudents}`;
-    for (const [field, names] of Object.entries(students)) {
-      result += `\nNumber of students in ${field}: ${names.length}. List: ${names.join(', ')}`;
-    }
-
-    return result;
-  } catch (error) {
-    throw new Error('Cannot load the database');
-  }
-}
+const countStudents = require('./3-read_file_async');
 
 const app = http.createServer(async (req, res) => {
   res.statusCode = 200;
