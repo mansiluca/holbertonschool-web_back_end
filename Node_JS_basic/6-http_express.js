7ff18978f077751dfc882333cbb47d30e9a1de80
mansiluca <49164932+mansiluca@users.noreply.github.com>
2025-05-21 00:08:47 +0200

    Refactor 6-http_express.js to improve server configuration by using constants for hostname and port; adjust formatting for consistency

--- Node_JS_basic/6-http_express.js
+++ Node_JS_basic/6-http_express.js
@@ -1,13 +1,15 @@
 const express = require('express');
 
 const app = express();
+const hostname = '127.0.0.1';
+const port = 1245;
 
 app.get('/', (req, res) => {
-  res.send('Hello Holberton School!');
+    res.send('Hello Holberton School!');
 });
 
-app.listen(1245, () => {
-  console.log('Server listening on port 1245');
+app.listen(port, hostname, () => {
+    // console.log(`Server running at http://${hostname}:${port}/`);
 });
 
 module.exports = app;
