fb3ba65e757762fca46ab5a4146df4ef3173b72d
mansiluca <49164932+mansiluca@users.noreply.github.com>
2025-05-20 10:46:53 +0200

    Refactor stdin data handling for improved readability and structure

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,8 +1,13 @@
 console.log('Welcome to Holberton School, what is your name?');
-process.stdin.on('data', (data) => {
-  const name = data.toString().trim();
-  console.log(`Your name is: ${name}`);
+process.stdin.setEncoding('utf8');
+process.stdin.on('readable', () => {
+  const chunk = process.stdin.read();
+  if (chunk !== null) {
+    const name = chunk.trim();
+    console.log(`Your name is: ${name}`);
+  }
 });
+
 process.stdin.on('end', () => {
   console.log('This important software is now closing');
 });
