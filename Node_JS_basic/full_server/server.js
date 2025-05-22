67491fa6e78e093e5832ff6b16c962c0ddee86b2
mansiluca <49164932+mansiluca@users.noreply.github.com>
2025-05-22 11:02:56 +0200

    Refactor controllers and utils for improved readability and consistency; adjust formatting and remove unnecessary newlines

--- Node_JS_basic/full_server/server.js
+++ Node_JS_basic/full_server/server.js
@@ -1,9 +1,9 @@
-import express from "express";
-import Routing from "./routes/index";
+import express from 'express';
+import Routing from './routes/index';
 
 const app = express();
 const port = 1245;
-const host = "127.0.0.1";
+const host = '127.0.0.1';
 
 Routing(app);
 
