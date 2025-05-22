d27c658ffb134fd44e7eff3ff00a2c99c438a1a2
mansiluca <49164932+mansiluca@users.noreply.github.com>
2025-05-22 10:58:23 +0200

    Implement initial server setup and routing for student data retrieval

---
+++ Node_JS_basic/full_server/controllers/StudentsController.js
@@ -0,0 +1,37 @@
+import readDatabase from '../utils';
+
+class StudentsController {
+    static getAllStudents(request, response) {
+        readDatabase(process.argv[2]).then((data) => {
+            const output = ['This is the list of our students'];
+            
+            const fields = Object.keys(data).sort((a, b) => a.toLowerCase().localeCompare(b.toLowerCase()));
+            
+            for (const field of fields) {
+                const students = data[field];
+                output.push(`Number of students in ${field}: ${students.length}. List: ${students.join(', ')}`);
+            }
+            
+            response.status(200).send(output.join('\n'));
+        }).catch(() => {
+            response.status(500).send('Cannot load the database');
+        });
+    }
+
+    static getAllStudentsByMajor(request, response) {
+        const { major } = request.params;
+        
+        if (major !== 'CS' && major !== 'SWE') {
+            return response.status(500).send('Major parameter must be CS or SWE');
+        }
+        
+        readDatabase(process.argv[2]).then((data) => {
+            const students = data[major] || [];
+            response.status(200).send(`List: ${students.join(', ')}`);
+        }).catch(() => {
+            response.status(500).send('Cannot load the database');
+        });
+    }
+}
+
+export default StudentsController;
